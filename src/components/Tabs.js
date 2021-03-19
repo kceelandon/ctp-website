6c6318c3e4cdfd4419142785ce983ecec6f27a60
Kcee Landon <landonkcee@gmail.com>
2021-03-19 14:31:01 -0700

    start of new session

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -7,6 +7,7 @@ import Recruitment from '../pages/Recruitment'
 import Contact from '../pages/Contact'
 import Philanthropy from '../pages/Philanthropy'
 import '../styles/Tabs.css'
+import '../index.css'
 
 function Tabs(props) {
     /* TODO:
@@ -18,16 +19,16 @@ function Tabs(props) {
             <div className="row">
                 <div className="col-md-12">
                     <Router>
-                        <Navbar style={{backgroundColor: 'yellow'}} variant="dark" fixed="top">
-                            <Navbar.Brand style={{color: 'black'}} href="/">Chi Theta Psi</Navbar.Brand>
+                        <Navbar style={{backgroundColor: '#F8BC31'}} variant="dark" fixed="top">
+                            <Navbar.Brand style={{color: '#3F3939', fontFamily: 'Nobel'}} href="/">Chi Theta Psi</Navbar.Brand>
                             <Navbar.Toggle aria-controls="basic-navbar-nav" />
                             <Navbar.Collapse id="basic-navbar-nav">
                                 <Nav className="m-auto">
-                                    <Nav.Link className="mr-2" style={{color: 'black'}} href="/">Home</Nav.Link>
-                                    <Nav.Link className="mr-2" style={{color: 'black'}} href="/about">About Us</Nav.Link>
-                                    <Nav.Link className="mr-2" style={{color: 'black'}} href="/recruitment">Recruitment</Nav.Link>
-                                    <Nav.Link className="mr-2" style={{color: 'black'}} href="/philanthropy">Philanthropy</Nav.Link>
-                                    <Nav.Link style={{color: 'black'}} href="/contact">Contact Us</Nav.Link>
+                                    <Nav.Link className="mr-2" style={{color: '#3F3939'}} href="/">Home</Nav.Link>
+                                    <Nav.Link className="mr-2" style={{color: '#3F3939'}} href="/about">About Us</Nav.Link>
+                                    <Nav.Link className="mr-2" style={{color: '#3F3939'}} href="/recruitment">Recruitment</Nav.Link>
+                                    <Nav.Link className="mr-2" style={{color: '#3F3939'}} href="/philanthropy">Philanthropy</Nav.Link>
+                                    <Nav.Link style={{color: '#3F3939'}} href="/contact">Contact Us</Nav.Link>
                                 </Nav>
                             </Navbar.Collapse>
                         </Navbar>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,9 @@
+import '../styles/Home.css'
 
 function Home(props) {
     return (
         <div>
-            <h1>This is the Home page!</h1>
+            <h1 className='first-header'>This is the Home page!</h1>
         </div>
     );
 }
