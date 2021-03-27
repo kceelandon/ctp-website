12002c1474d538040a35eb633e4dc31c1fb05206
Kcee Landon <landonkcee@gmail.com>
2021-03-27 15:41:45 -0700

    added some routing templating to app

--- src/App.js
+++ src/App.js
@@ -6,6 +6,20 @@ function App() {
   return (
     <div className="App">
         <Tabs/>
+        <Router>
+          <Route path="/">
+            {/* insert home page component(s) here */}
+          </Route>
+          <Route path="/about">
+            {/* insert about page component(s) here */}
+          </Route>
+          <Route path="/recruitment">
+            {/* insert recruitment page component(s) here */}
+          </Route>
+          <Route path="/philanthropy">
+            {/* insert philanthropy page component(s) here */}
+          </Route>
+        </Router>
     </div>
   );
 }
